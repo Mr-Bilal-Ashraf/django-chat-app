19d236e2a9d937183d250b25f9b9355eabdcf4b2
Muhammad Bilal Azaad <mr.bilal2066@gmail.com>
2023-11-22 18:24:50 +0500

    feat: added channels layer

--- ChatProject/settings.py
+++ ChatProject/settings.py
@@ -68,6 +68,15 @@ TEMPLATES = [
 # WSGI_APPLICATION = 'ChatProject.wsgi.application'
 ASGI_APPLICATION = "ChatProject.asgi.application"
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
