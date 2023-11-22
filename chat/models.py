1f250d1daff38f0d7a5c52c602f0172994b5ece1
Muhammad Bilal Azaad <mr.bilal2066@gmail.com>
2023-11-22 18:25:37 +0500

    feat: chat models added

--- chat/models.py
+++ chat/models.py
@@ -5,24 +5,27 @@ User = get_user_model()
 
 
 class Conversation(models.Model):
-    initiator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='initiator')
-    participant = models.ForeignKey(User, on_delete=models.CASCADE, related_name='participant')
+    initiator = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="initiator"
+    )
+    participant = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="participant"
+    )
 
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.initiator} - {self.participant} on {self.created_at}'
+        return f"{self.initiator} - {self.participant} on {self.created_at}"
 
 
 class Message(models.Model):
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sender")
     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
     text = models.TextField(blank=True)
     send_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.sender} - {self.text} ({self.send_at})'
+        return f"{self.sender} - {self.text} ({self.send_at})"
 
     class Meta:
-        ordering = ['send_at']
-
+        ordering = ["send_at"]
