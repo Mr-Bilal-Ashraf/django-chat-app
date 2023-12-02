ca242b2dfbdc004c1a1dc0ce8884dec4bf12ee22
Muhammad Bilal Azaad <mr.bilal2066@gmail.com>
2023-12-02 22:30:53 +0500

    feat: redirect to conversation if authenticated

--- user/views.py
+++ user/views.py
@@ -3,3 +3,11 @@ from django.views.generic import TemplateView
 
 class SignUpView(TemplateView):
     template_name = 'user/authentication.html'
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect(reverse("chat:index"))
+        return super().dispatch(request, *args, **kwargs)
