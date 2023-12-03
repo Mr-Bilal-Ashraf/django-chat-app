10c6b7644f967df68912bac5772dcb11a0d7b575
Muhammad Bilal Azaad <mr.bilal2066@gmail.com>
2023-12-03 16:23:11 +0500

    feat: get friends list API

--- chat/urls.py
+++ chat/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from chat.views import IndexView
+from chat.views import IndexView, FriendListAPI
 
 app_name = "chat"
 
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("friends/", FriendListAPI.as_view(), name="friends"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,20 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from django.contrib.auth import get_user_model
 from django.views.generic import TemplateView
+
+from user.serializers import UserSerializer
+
+User = get_user_model()
 
 
 class IndexView(TemplateView):
     template_name = "chat/index.html"
+
+
+class FriendListAPI(APIView):
+    def get(self, request):
+        friends = request.user.friends.all()
+        serializer = UserSerializer(friends, many=True)
+        return Response(serializer.data)
